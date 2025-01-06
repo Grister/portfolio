5601c5c22d04e2eba5a59498a2820bbfb51ff1cc
grister <kravchenko.cotleto@gmail.com>
2025-01-06 15:53:34 +0200

    added adaptation and js pagination

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
 const root = document.documentElement;
 const themeSwitcher = document.getElementById("themeSwitch");
+const savedTheme = localStorage.getItem('theme');
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.menu__close');
 
-themeSwitcher.addEventListener('click', (e) => {
-    if (e.target.checked == true) {
+function toggleTheme(isDark) {
+    if (isDark) {
         root.style.setProperty('--background-color', '#0D0D0D');
         root.style.setProperty('--main-color', '#F0F0F0');
         root.style.setProperty('--shadow', '9px 5px 30px 0 #7C8188');
@@ -13,4 +17,31 @@ themeSwitcher.addEventListener('click', (e) => {
         root.style.setProperty('--shadow', '18px 18px 30px 0px #d1d9e6, -13px -13px 30px 0 #fff');
         root.style.setProperty('--card-color', '#F0F0F0b3');
     }
+};
+
+if (savedTheme === 'dark') {
+    themeSwitcher.checked = true;
+    toggleTheme(true);
+} else {
+    themeSwitcher.checked = false;
+    toggleTheme(false);
+}
+
+themeSwitcher.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        toggleTheme(true);
+        localStorage.setItem('theme', 'dark');
+    } else {
+        toggleTheme(false);
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+
+burger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
 });
